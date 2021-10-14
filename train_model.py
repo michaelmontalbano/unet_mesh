5556d78324cda0b2987673c1001a05e7bbb711d3
michaelmontalbano <90255492+michaelmontalbano@users.noreply.github.com>
2021-10-14 10:10:01 -0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -10,24 +10,12 @@ from tensorflow.keras.layers import InputLayer
 from tensorflow.keras.layers import Convolution2D, Dense, MaxPooling2D, Flatten, BatchNormalization, Dropout
 from tensorflow.keras import Sequential
 from sklearn.model_selection import train_test_split
-import random, argparse
-import re
-import png
-import sklearn.metrics
-import os
-import fnmatch
-import re
-import numpy as np
+import random, argparse, re, png, sklearn.metrics, sys, pickle, time
 from tensorflow.python.ops.numpy_ops.np_math_ops import true_divide
 from unet import *
 from job_control import *
-import sys
-import pickle
-import time
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
-
-
 
 # gpus = tf.config.experimental.list_physical_devices('GPU') 
 # tf.config.experimental.set_memory_growth(gpus[0], True)
@@ -35,14 +23,15 @@ print('hi')
 strategy = tf.distribute.MirroredStrategy()
 print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
-
+twice = False
 def create_parser():
-    parser = argparse.ArgumentParser(description='Hail Swath Learner')
-    parser.add_argument('-exp_type',type=str,default='minimum',help='How to name this model?')
-    parser.add_argument('-thres', type=float, default=0.25, help='Enter the dropout rate.' )
+    parser = argparse.ArgumentParser(description='Hail Swath Learner')    
+    parser.add_argument('-kind',type=str,default='raw',help='How is the data cleaned? (norm, binary, multiclass)')
+    parser.add_argument('-exp_type',type=str,default='two_layers',help='How to name this model?')
+    parser.add_argument('-thres', type=float, default=None, help='Enter the dropout rate.' )
     parser.add_argument('-dropout', type=float, default=None, help='Enter the dropout rate.' )
     parser.add_argument('-lambda_regularization', type=float, default=0.1, help='Enter l1, l2, or none.') 
-    parser.add_argument('-epochs', type=int, default=320, help='Training epochs')
+    parser.add_argument('-epochs', type=int, default=150, help='Training epochs')
     parser.add_argument('-results_path', type=str, default='~/MESH_results', help='Results directory')
     parser.add_argument('-lrate', type=float, default=0.001, help="Learning rate")
     parser.add_argument('-patience', type=int, default=100 , help="Patience for early termination")   
@@ -52,7 +41,6 @@ def create_parser():
     parser.add_argument('-batch_size',type=int, default=50, help='Enter the batch size.')
     parser.add_argument('-activation',type=str, default='relu', help='Enter the activation function.')
     parser.add_argument('-optimizer',type=str, default='adam', help='Enter the optimizer.')
-    parser.add_argument('-kind',type=str,default='raw_Min',help='How is the data cleaned? (norm, binary, multiclass)')
     parser.add_argument('-exp_index', nargs='+', type=int, help='Array of integers')
     parser.add_argument('-type',type=str,default='regression',help='How type')
     parser.add_argument('-error',type=str,default='mse')
@@ -109,13 +97,15 @@ def transform(var):
 def transform_test(var,scalers):
     n_channels=var.shape[1]
     tdata_transformed = np.zeros_like(var)
-
+    channel_scalers = []
     for i in range(n_channels):
+        mmx = StandardScaler()
         slc = var[:, i, :, :].reshape(var.shape[0], 60*60)
-        transformed = scalers[i].transform(slc)
+        transformed = mmx.fit_transform(slc)
         transformed = transformed.reshape(var.shape[0], 60,60) # reshape it back to tiles
         tdata_transformed[:, i, :, :] = transformed # put it in the transformed array
-    return tdata_transformed
+        channel_scalers.append(mmx)
+    return tdata_transformed, channel_scalers
 
 
 def training_set_generator_images(ins, outs, batch_size=10,
@@ -139,11 +129,14 @@ def training_set_generator_images(ins, outs, batch_size=10,
         yield({input_name: ins[example_indices,:,:,:]},
              {output_name: outs[example_indices,:,:,:]})
 
+# Standard MSE loss function plus term penalizing only misses
+def my_MSE_fewer_misses ( y_true, y_pred ):
+    return K.square(y_pred - y_true) + K.maximum((y_true - y_pred-20), 0) + K.maximum((y_pred - y_true + 10),0)
+
+
 parser = create_parser()
 args = parser.parse_args()
 
-#ins,outs = load_data(cases_df, 451)
-# ins_raw ins_raw_multi ins_raw_noNSE
 ins = np.load('ins_{}.npy'.format(args.kind))
 #np.save('ins_raw_no3D.npy',ins[:,:21,:,:])
 outs = np.load('outs_raw.npy'.format(args.kind))
@@ -158,15 +151,16 @@ ins_train_indices, ins_test_indices , outs_train_indices, outs_test_indices = tr
 # scaling
 ins_train, scalers = transform(ins_train)
 # ins_val = transform_test(ins_val,scalers)
-ins_test = transform_test(ins_test,scalers)
+ins_test, scalars = transform_test(ins_test,scalers)
 
 
 outs_train, scalers = transform(outs_train)
 # outs_val = transform_test(outs_val,scalers)
-outs_test = transform_test(outs_test,scalers)
+outs_test, scalers = transform_test(outs_test,scalers)
 
 # save 
 pickle.dump(scalers, open('scaler_{}.pkl'.format(args.kind),'wb'))
+#pickle.dump(scalers, open('scaler_raw_noShear.pkl','wb'))
 
 with strategy.scope():
     model = create_uNet(ins_train.shape[1:], nclasses=5,lambda_regularization=args.lambda_regularization,
@@ -197,13 +191,9 @@ history = model.fit(x=generator,
 #                    validation_data=(ins_val, outs_val),
                     verbose=True, 
                     callbacks=[early_stopping_cb])
-
-model.compile(loss=my_MSE_fewer_misses, metrics='mse')
-
-history = model.fit(
-[x_train_vis,x_train_ir], y_train,
-validation_data=([x_test_vis,x_test_ir], y_test),
-epochs=100, batch_size=10)
+if twice == True:
+    model.compile(loss=my_MSE_fewer_misses, metrics='mse')
+    history = model.fit(ins_train, outs_train,epochs=100, batch_size=370)
 
 
 results = {}
